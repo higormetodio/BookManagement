06f2041205b275953ebcddd874b46573dade695c
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:37:13 -0300

    Implementing book repository

---
+++ BoockManagement.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,54 @@
+﻿using BookManagement.Core.Entities;
+using BookManagement.Core.Interfaces;
+using BookManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Infrastructure.Repositories;
+public class BookRepository : IBookRepository
+{
+    private readonly BookManagementDbContext _context;
+
+    public BookRepository(BookManagementDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Book>> GetAllBooksAsync()
+    {
+        var books = await _context.Books.Include(s => s.Stock)
+                                        .AsNoTracking()
+                                        .ToListAsync();
+
+        return books;
+    }
+
+    public async Task<Book> GetBookByIdAsync(int id)
+        => await _context.Books.Include(s => s.Stock)
+                               .SingleOrDefaultAsync(b => b.Id == id);
+
+    public async Task<Book> GetBookLoansByIdAsync(int id)
+        => await _context.Books.Include(b => b.Loans)
+                               .ThenInclude(l => l.User)
+                               .Include(b => b.Stock)
+                               .SingleOrDefaultAsync(b => b.Id == id);
+
+    public async Task<int> CreateBookAsync(Book book)
+    {
+        await _context.Books.AddAsync(book);
+        await _context.SaveChangesAsync();
+
+        return book.Id;
+    }
+
+    public async Task UpdateBookStockAsync(BookStock stock)
+    {
+         _context.BookStocks.Update(stock);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateBookAsync(Book book)
+    {
+        _context.Books.Update(book);
+        await _context.SaveChangesAsync();
+    }
+}
