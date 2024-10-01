40f1f27b1221ee5a9e62810a8dcae88b4d8ac68a
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:42:01 -0300

    Creating extension method InfrastructureModule for implementing DI in the infrastructure layer

---
+++ BoockManagement.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,22 @@
+﻿using BookManagement.Core.Interfaces;
+using BookManagement.Infrastructure.Persistence;
+using BookManagement.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Infrastructure;
+public static class InfrastructureModule
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<BookManagementDbContext>(options =>
+                             options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING", EnvironmentVariableTarget.User), b =>
+                                                 b.MigrationsAssembly(typeof(BookManagementDbContext).Assembly.FullName)));
+
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ILoanRepository, LoanRepository>();
+
+        return services;
+    }
+}
