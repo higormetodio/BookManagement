83b92188afacbe9cd9acec9236bc57efbf2afdd2
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:35:53 -0300

    Creating context class BookManagementDbContext

---
+++ BoockManagement.Infrastructure/Persistence/BookManagementDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BookManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Infrastructure.Persistence;
+public class BookManagementDbContext : DbContext
+{
+    public BookManagementDbContext(DbContextOptions<BookManagementDbContext> options) : base(options)
+    { }
+
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<BookStock> BookStocks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(BookManagementDbContext).Assembly);
+    }
+
+}
