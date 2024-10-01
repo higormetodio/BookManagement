3a387e07f90350f24b3febf091dc2125d4a40734
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:33:47 -0300

    Implementing EF Core configurantig for books stock

---
+++ BoockManagement.Infrastructure/EntitiesConfiguration/BookStockConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using BookManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManagement.Infrastructure.EntitiesConfiguration;
+public class BookStockConfiguration : IEntityTypeConfiguration<BookStock>
+{
+    public void Configure(EntityTypeBuilder<BookStock> builder)
+    {
+        builder.ToTable("BookStock");
+
+        builder.HasKey(s => s.Id);
+        builder.Property(s => s.Id)
+               .ValueGeneratedOnAdd()
+               .UseIdentityColumn();
+
+        builder.Property(s => s.Quantity)
+               .IsRequired()
+               .HasColumnType("INT");
+
+        builder.Property(s => s.LoanQuantity)
+               .IsRequired()
+               .HasColumnType("INT");
+
+        builder.Property(s => s.BookId)
+               .IsRequired()
+               .HasColumnType("INT");
+
+        builder.HasOne(s => s.Book)
+               .WithOne(b => b.Stock)
+               .HasForeignKey<BookStock>(s => s.BookId)
+               .HasConstraintName("FK_BookStocks_Book")
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
