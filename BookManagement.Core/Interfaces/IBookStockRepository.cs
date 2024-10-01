a7eb6ea128a67c197c979f2f8a40d4dd82fa235f
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:29:38 -0300

    Removing interface IBookStockRepository

--- BookManagement.Core/Interfaces/IBookStockRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BookManagement.Core.Entities;
-
-namespace BookManagement.Core.Interfaces;
-public interface IBookStockRepository
-{
-    Task<IEnumerable<BookStock>> GetAllBookStocksAsync();
-    Task<BookStock> GetBookStockByBookIdAsync(int id);
-    Task UpdateBookStockAsync(BookStock stock, int quantity);
-}
