61e99a69fb5c696b37a69bd340c6297dba824a07
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:34:28 -0300

    Implementing EF Core configurantig for user

---
+++ BoockManagement.Infrastructure/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using BookManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManagement.Infrastructure.EntitiesConfiguration;
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id)
+               .ValueGeneratedOnAdd()
+               .UseIdentityColumn();
+
+        builder.Property(u => u.Name)
+               .IsRequired()
+               .HasColumnType("NVARCHAR")
+               .HasMaxLength(100);
+
+        builder.Property(u => u.Email)
+               .IsRequired()
+               .HasColumnType("NVARCHAR")
+               .HasMaxLength(50);
+
+        builder.Property(u => u.Active)
+               .IsRequired()
+               .HasColumnType("BIT");
+
+        builder.HasIndex(u => u.Name)
+               .HasDatabaseName("IX_Users_Name")
+               .IsUnique();
+
+        builder.HasIndex(u => u.Email)
+               .HasDatabaseName("IX_Users_Email")
+               .IsUnique();
+    }
+}
