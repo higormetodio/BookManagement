a855876dd692a029b9157e2c1abaf524fdb445fc
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:24:20 -0300

    Changing entity class BookStock

--- BookManagement.Core/Entities/BookStock.cs
+++ BookManagement.Core/Entities/BookStock.cs
@@ -6,10 +6,12 @@ public class BookStock : BaseEntity
     public BookStock(int quantity, int bookId)
     {
         Quantity = quantity;
+        LoanQuantity = 0;
         BookId = bookId;
     }
 
     public int Quantity { get; private set; }
+    public int LoanQuantity { get; private set; }
     public int BookId { get; private set; }
     public Book? Book { get; private set; }
 
@@ -17,7 +19,19 @@ public class BookStock : BaseEntity
     public void Update(int quantity)
     {
         CoreExceptionValidation.When(quantity < 0, "Invalid Quantity. The Quantity must be greater than or equal to zero.");
-        
+
         Quantity = quantity;
+    }
+
+    public void LoanStockMovement()
+    {
+        Quantity -= 1;
+        LoanQuantity += 1;
+    }
+
+    public void ReturnStockMovement()
+    {
+        Quantity += 1;
+        LoanQuantity -= 1;
     }
 }
