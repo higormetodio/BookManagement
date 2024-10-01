7986e4c9a94cd8d2c7674280a2c57e6e22d7be16
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:27:45 -0300

    Removing status Canceled of the enum LoanStatus

--- BookManagement.Core/Enums/LoanStatus.cs
+++ BookManagement.Core/Enums/LoanStatus.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BookManagement.Core.Enums;
+﻿namespace BookManagement.Core.Enums;
 public enum LoanStatus
 {
     Loaned = 1,
     Returned = 2,
-    Late = 3,
-    Canceled = 4
+    Late = 3
 }
