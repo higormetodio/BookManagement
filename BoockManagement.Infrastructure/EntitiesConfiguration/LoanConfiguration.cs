4da6bb589aba2bf170b2144ab7e7aa6179bbd893
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:34:09 -0300

    Implementing EF Core configurantig for loan

---
+++ BoockManagement.Infrastructure/EntitiesConfiguration/LoanConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using BookManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManagement.Infrastructure.EntitiesConfiguration;
+public class LoanConfiguration : IEntityTypeConfiguration<Loan>
+{
+    public void Configure(EntityTypeBuilder<Loan> builder)
+    {
+        builder.ToTable("Loans");
+
+        builder.HasKey(l => l.Id);
+        builder.Property(l => l.Id)
+               .ValueGeneratedOnAdd()
+               .UseIdentityColumn();
+
+        builder.Property(l => l.UserId)
+               .IsRequired()
+               .HasColumnType("INT");
+
+        builder.Property(l => l.BookId)
+               .IsRequired()
+               .HasColumnType("INT");
+
+        builder.Property(l => l.LoanDate)
+               .IsRequired()
+               .HasColumnType("SMALLDATETIME")
+               .HasMaxLength(50);
+
+        builder.Property(l => l.ReturnDate)
+               .IsRequired()
+               .HasColumnType("SMALLDATETIME")
+               .HasMaxLength(50);
+
+        builder.Property(l => l.Status)
+               .IsRequired()
+               .HasColumnType("INT");
+
+        builder.HasOne(l => l.Book)
+               .WithMany(b => b.Loans)
+               .HasForeignKey(l => l.BookId)
+               .HasConstraintName("FK_Loan_Book")
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(l => l.User)
+               .WithMany(u => u.Loans)
+               .HasForeignKey(l => l.UserId)
+               .HasConstraintName("FK_Loans_User")
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
