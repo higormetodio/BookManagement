73fce3cd7abbb4fc7e153cf6cd22513f0b9213a1
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:33:22 -0300

    Implementing EF Core configurantig for books

---
+++ BoockManagement.Infrastructure/EntitiesConfiguration/BookConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using BookManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManagement.Infrastructure.EntitiesConfiguration;
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.ToTable("Books");
+
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.Id)
+               .ValueGeneratedOnAdd()
+               .UseIdentityColumn();
+
+        builder.Property(b => b.Title)
+               .IsRequired()
+               .HasColumnType("NVARCHAR")
+               .HasMaxLength(200);
+
+        builder.Property(b => b.Author)
+               .IsRequired()
+               .HasColumnType("NVARCHAR")
+               .HasMaxLength(100);
+
+        builder.Property(b => b.ISBN)
+               .IsRequired()
+               .HasColumnType("NVARCHAR")
+               .HasMaxLength(20);                                     
+
+        builder.Property(b => b.PublicationYear)
+               .IsRequired()
+               .HasColumnType("INT");
+
+        builder.Property(b => b.IsReserved)
+               .IsRequired()
+               .HasColumnType("BIT");
+
+        builder.Property(b => b.Active)
+               .IsRequired()
+               .HasColumnType("BIT");
+
+        builder.HasIndex(b => b.ISBN)
+               .HasName("IX_Book_ISBN")
+               .IsUnique();
+    }
+}
