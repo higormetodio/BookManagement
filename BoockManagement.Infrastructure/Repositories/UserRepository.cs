3a0a64dd2ba13e505353a79f49f147f203423eb8
Higor Metodio <higor.metodio@outlook.com>
2024-10-01 15:37:53 -0300

    Implementing user repository

---
+++ BoockManagement.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using BookManagement.Core.Entities;
+using BookManagement.Core.Interfaces;
+using BookManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagement.Infrastructure.Repositories;
+public class UserRepository : IUserRepository
+{
+    private readonly BookManagementDbContext _context;
+
+    public UserRepository(BookManagementDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsersAsync()
+        => await _context.Users.AsNoTracking().ToListAsync();
+
+    public async Task<User> GetUserByIdAsync(int id)
+        => await _context.Users.FindAsync(id);
+
+    public async Task<User> GetUserLoansByIdAsync(int id)
+        => await _context.Users.Include(u => u.Loans)
+                               .ThenInclude(l => l.Book)
+                               .SingleOrDefaultAsync(u => u.Id == id);
+
+    public async Task<int> CreateUserAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        return user.Id;
+
+    }
+
+    public async Task UpdateUserAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+    }
+}
