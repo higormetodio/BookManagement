1f314b3d30c765dbf8e7090b7890b466472e7ea2
Higor Metodio <higor.metodio@outlook.com>
2024-10-08 18:38:54 -0300

    Add new properties for password, birth date and role.

--- BoockManagement.Infrastructure/EntitiesConfiguration/UserConfiguration.cs
+++ BoockManagement.Infrastructure/EntitiesConfiguration/UserConfiguration.cs
@@ -24,9 +24,22 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
                .HasColumnType("NVARCHAR")
                .HasMaxLength(50);
 
+        builder.Property(u => u.BirthDate)
+               .IsRequired()
+               .HasColumnType("SMALLDATETIME");
+
         builder.Property(u => u.Active)
                .IsRequired()
                .HasColumnType("BIT");
+
+        builder.Property(u => u.Password)
+               .IsRequired()
+               .HasColumnType("NVARCHAR(MAX)");
+
+        builder.Property(u => u.Role)
+               .IsRequired()
+               .HasColumnType("NVARCHAR")
+               .HasMaxLength(20);
 
         builder.HasIndex(u => u.Name)
                .HasDatabaseName("IX_Users_Name")
