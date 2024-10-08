38f9e3edfc3e7a145d29fc2f4b16693209dc767f
Higor Metodio <higor.metodio@outlook.com>
2024-10-08 18:37:45 -0300

    New validations for password and birthdate

--- BoockManagement.Application/Validators/CreateUserValidator.cs
+++ BoockManagement.Application/Validators/CreateUserValidator.cs
@@ -1,5 +1,6 @@
 ﻿using BookManagement.Application.Commands.CreateUser;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace BookManagement.Application.Validators;
 public class CreateUserValidator : AbstractValidator<CreateUserCommand>
@@ -10,5 +11,17 @@ public class CreateUserValidator : AbstractValidator<CreateUserCommand>
                             .MaximumLength(100);
 
         RuleFor(u => u.Email).EmailAddress().WithMessage("Invalid Email.");
+
+        RuleFor(u => u.Password).Must(ValidPassword)
+                                .WithMessage("Password must contain at least 8 characters, one number, one capital letter, one lowercase letter, and one special character.");
+
+        RuleFor(u => u.BirthDate).GreaterThanOrEqualTo(new DateTime(1900, 1, 1));
+    }
+
+    public bool ValidPassword(string password)
+    {
+        var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+
+        return regex.IsMatch(password);
     }
 }
