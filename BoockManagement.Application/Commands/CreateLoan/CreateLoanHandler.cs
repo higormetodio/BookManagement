9f27cbf2501d407270ed932978f0d3045b86f92f
Higor Metodio <higor.metodio@outlook.com>
2024-10-08 18:40:23 -0300

    Adjustments to the logic of creating a loan

--- BoockManagement.Application/Commands/CreateLoan/CreateLoanHandler.cs
+++ BoockManagement.Application/Commands/CreateLoan/CreateLoanHandler.cs
@@ -5,30 +5,43 @@ using MediatR;
 namespace BookManagement.Application.Commands.CreateLoan;
 public class CreateLoanHandler : IRequestHandler<CreateLoanCommand, ResultViewModel<int>>
 {
-    private readonly ILoanRepository _repositoryLoan;
-    private readonly IBookRepository _repositoryBook;
+    private readonly ILoanRepository _loanRepository;
+    private readonly IBookRepository _bookRepository;
+    private readonly IUserRepository _userRepository;
 
-    public CreateLoanHandler(ILoanRepository repository, IBookRepository repositoryBook)
+    public CreateLoanHandler(ILoanRepository repository, IBookRepository repositoryBook, IUserRepository userRepository)
     {
-        _repositoryLoan = repository;
-        _repositoryBook = repositoryBook;
+        _loanRepository = repository;
+        _bookRepository = repositoryBook;
+        _userRepository = userRepository;
     }
 
     public async Task<ResultViewModel<int>> Handle(CreateLoanCommand request, CancellationToken cancellationToken)
     {
         var loan = request.ToEntity();
 
-        var book = await _repositoryBook.GetBookByIdAsync(loan.BookId);
+        var user = await _userRepository.GetUserByIdAsync(request.UserId);
+        var book = await _bookRepository.GetBookByIdAsync(loan.BookId);
 
+        if (user is null || !user.Active)
+        {
+            return ResultViewModel<int>.Error("User not found.");
+        }
+
+        if (book is null || !book.Active)
+        {
+            return ResultViewModel<int>.Error("Book not found.");
+        }
+        
         if (book.Stock.Quantity <= 0)
         {
             return ResultViewModel<int>.Error("There is no book available.");
         }
 
-        await _repositoryLoan.CreateLoanAsync(loan);        
+        await _loanRepository.CreateLoanAsync(loan);        
         
         book.Stock.LoanStockMovement();
-        await _repositoryBook.UpdateBookStockAsync(book.Stock);
+        await _bookRepository.UpdateBookStockAsync(book.Stock);
 
         return ResultViewModel<int>.Success(loan.Id);
     }
