964a6d551720a5289534a1d0ca2fa6c6f043b985
Higor Metodio <higor.metodio@outlook.com>
2024-10-08 18:36:43 -0300

    Add creating password hash

--- BoockManagement.Application/Commands/CreateUser/CreateUserCommand.cs
+++ BoockManagement.Application/Commands/CreateUser/CreateUserCommand.cs
@@ -1,13 +1,18 @@
 ﻿using BookManagement.Application.Models;
 using BookManagement.Core.Entities;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace BookManagement.Application.Commands.CreateUser;
 public class CreateUserCommand : IRequest<ResultViewModel<int>>
 {
     public string Name { get; set; }
     public string Email { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Password { get; set; }
+    [JsonIgnore]
+    public string Role { get; set; } = "user";
 
-    public User ToEntity()
-        => new(Name, Email);
+    public User ToEntity(string password)
+        => new(Name, Email, password, Role, BirthDate);
 }
--- BoockManagement.Application/Commands/CreateUser/CreateUserHandler.cs
+++ BoockManagement.Application/Commands/CreateUser/CreateUserHandler.cs
@@ -1,20 +1,25 @@
 ﻿using BookManagement.Application.Models;
 using BookManagement.Core.Repositories;
+using BookManagement.Core.Services;
 using MediatR;
 
 namespace BookManagement.Application.Commands.CreateUser;
 public class CreateUserHandler : IRequestHandler<CreateUserCommand, ResultViewModel<int>>
 {
     private readonly IUserRepository _repository;
+    private readonly IAuthService _authService;
 
-    public CreateUserHandler(IUserRepository repository)
+    public CreateUserHandler(IUserRepository repository, IAuthService authService)
     {
         _repository = repository;
+        _authService = authService;
     }
 
     public async Task<ResultViewModel<int>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
-        var user = request.ToEntity();
+        var password = _authService.ComputeSha256Hash(request.Password);
+
+        var user = request.ToEntity(password);
 
         await _repository.CreateUserAsync(user);
 
