f2769423dc488115264e0747a13799639768d999
Higor Metodio <higor.metodio@outlook.com>
2024-10-18 18:16:01 -0300

    Creating a Test In Memory as DbContext.

---
+++ BoockManagement.Infrastructure/Persistence/TestsInMemoryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Infrastructure.Persistence;
+public class TestsInMemoryDbContext
+{
+    public BookManagementDbContext CreateTestsDbContext()
+    {
+        var options = new DbContextOptionsBuilder<BookManagementDbContext>().UseInMemoryDatabase("TestesInMemoryDatabase").Options;
+
+        var testsDbContext = new BookManagementDbContext(options);
+
+        return testsDbContext;                                
+    }
+}
